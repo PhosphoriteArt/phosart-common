a86f5139a9e524d975891ef7f1160510eed1ea1d
Phosphorite <dew@phosphorite.art>
2025-12-30 17:22:23 -0800

    export fastcache

--- src/lib/server/index.ts
+++ src/lib/server/index.ts
@@ -4,7 +4,12 @@ export { type CharacterCache, characters } from './character.ts';
 export { $DATA as $ART, $PUBLIC, $ROOT } from './directories.ts';
 export { filter } from './filter.ts';
 export { type GalleryCache, type RawGalleryCache, galleries, rawGalleries } from './gallery.ts';
-export { processImage, processVideo } from './imageprocess.ts';
+export {
+	processImage,
+	processVideo,
+	processImageFastcache,
+	processVideoFastcache
+} from './imageprocess.ts';
 
 export { Artist } from './models/Artist.ts';
 export { FullCharacter, RawCharacter } from './models/Character.ts';
@@ -20,3 +25,4 @@ export {
 } from './models/Gallery.ts';
 export { Image, Picture, Source } from './models/image.ts';
 export { clearCache } from './util.ts';
+export { getFastHash, readFastCache, flushFastCache, updateFastCache } from './fastcache.ts';
