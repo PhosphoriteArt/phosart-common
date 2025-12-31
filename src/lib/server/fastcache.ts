99db1994c9d37bd372e5283f78d4422279790104
Phosphorite <dew@phosphorite.art>
2025-12-30 16:54:33 -0800

    fastcache

--- src/lib/server/fastcache.ts
+++ src/lib/server/fastcache.ts
@@ -1,6 +1,10 @@
-import { readFile, stat, writeFile } from 'node:fs/promises';
+import { rename, stat } from 'node:fs/promises';
 import { $DATA } from './directories.ts';
 import path from 'node:path';
+import { createGunzip, createGzip } from 'node:zlib';
+import { PackrStream, UnpackrStream } from 'msgpackr';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
 export interface FastCache {
 	[relpath: string]: {
 		mtime: number;
@@ -42,12 +46,31 @@ export async function updateFastCache(
 }
 
 export async function flushFastCache(fc: FastCache) {
-	await writeFile(path.join($DATA, '.fastcache.json'), JSON.stringify(fc), { encoding: 'utf-8' });
+	const final = path.join($DATA, '.fastcache.pack.gz');
+	const tmp = final + '.tmp';
+	const ws = createWriteStream(tmp);
+	const gz = createGzip({ level: 9 });
+	const packr = new PackrStream();
+	const p = pipeline(packr, gz, ws);
+	packr.end(fc);
+
+	await p;
+
+	await rename(tmp, final);
 }
 
 export async function readFastCache(): Promise<FastCache> {
 	try {
-		return JSON.parse(await readFile(path.join($DATA, '.fastcache.json'), { encoding: 'utf-8' }));
+		const gunzip = createGunzip();
+		const unpackr = new UnpackrStream();
+		const rs = createReadStream(path.join($DATA, '.fastcache.pack.gz'));
+		const data = new Promise<FastCache>((resolve) => {
+			unpackr.once('data', resolve);
+		});
+
+		await pipeline(rs, gunzip, unpackr);
+
+		return await data;
 	} catch {
 		return {};
 	}
