241cba6501c7caa392dd7dab41723f7de6e88d62
Phosphorite <dew@phosphorite.art>
2025-12-31 23:06:43 -0800

    try harder to find the root

--- src/lib/server/directories.ts
+++ src/lib/server/directories.ts
@@ -1,8 +1,8 @@
 import { join, dirname } from 'node:path/posix';
 import * as fs from 'node:fs';
 
-function findRoot() {
-	let cur = dirname(process.env.PROJECT_ROOT ?? process.argv[1]);
+function tryFindRoot(start: string) {
+	let cur = dirname(start);
 	while (cur.length > 1) {
 		if (cur.includes('node_modules')) {
 			cur = join(cur, '..');
@@ -17,7 +17,21 @@ function findRoot() {
 			cur = join(cur, '..');
 		}
 	}
-	throw new Error('Failed to find project root?');
+	return null;
+}
+
+function findRoot() {
+	let root: string | null = null;
+	if (process.env.PROJECT_ROOT) {
+		root = tryFindRoot(process.env.PROJECT_ROOT);
+	}
+	root = root ?? tryFindRoot(process.argv[1]) ?? tryFindRoot(process.cwd());
+
+	if (!root) {
+		throw new Error('Failed to find root');
+	}
+
+	return root;
 }
 
 export const $ROOT = join(findRoot(), 'src');
