300689c18da24a42f9e201a30e83059211e0a88a
Phosphorite <dew@phosphorite.art>
2026-01-02 18:01:25 -0800

    add combine

--- src/lib/util/util.ts
+++ src/lib/util/util.ts
@@ -1,11 +1,28 @@
-export function asRecord<T>(arr: T[], key: (el: T) => string): Record<string, T> {
+function defaultCombine<T>(a: T): T {
+	return a;
+}
+
+export function asRecord<T>(
+	arr: T[],
+	key: (el: T) => string,
+	combine?: (a: T, b: T) => T
+): Record<string, T> {
 	const record: Record<string, T> = {};
 	for (const val of arr) {
-		record[key(val)] = val;
+		const k = key(val);
+		if (k in record) {
+			record[k] = (combine ?? defaultCombine)(record[k], val);
+		} else {
+			record[k] = val;
+		}
 	}
 
 	return record;
 }
-export function deduplicateBy<T>(arr: T[], key: (el: T) => string): T[] {
-	return Object.values(asRecord(arr, key));
+export function deduplicateBy<T>(
+	arr: T[],
+	key: (el: T) => string,
+	combine?: (a: T, b: T) => T
+): T[] {
+	return Object.values(asRecord(arr, key, combine));
 }
